1b20edf57243388b7e5fdc8d3d67f992880aa813
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-08-26 21:55:57 -0400

    La Prueba para anio viciesto divicible entre 4 paso

--- src/AnioBiciesto.js
+++ src/AnioBiciesto.js
@@ -1,16 +1,12 @@
 function EsAnioBiciesto(numero)
 {
-    if(numero%100===0)
+    if(numero%4===0 && numero%100===0)
     {
-        if(numero%400===0)
-        {
-            return "Es Anio Biciesto";
-        }
-        else
-        {
-            return "No es Anio Biciesto";
-    
-        }
+        return "No es Anio Biciesto";
+    }
+    else if(numero%4===0)
+    {
+       return "Es Anio Biciesto";
     }
     
 }
--- src/AnioBiciesto.spec.js
+++ src/AnioBiciesto.spec.js
@@ -1,12 +1,16 @@
 import EsAnioBiciesto from "./AnioBiciesto";
 describe("Anio Biciesto",()=>{
-    it("deberia determinar si el anio 1600 es viciesto",()=>{
+    it("deberia determinar si el anio 2008 es viciesto",()=>{
         expect(EsAnioBiciesto(2008)).toEqual("Es Anio Biciesto");
     });
-    it("deberia determinar si el anio 1700 es viciesto",()=>{
-        expect(EsAnioBiciesto(2012)).toEqual("No es Anio Biciesto");
+    it("deberia determinar si el anio 2012 es viciesto",()=>{
+        expect(EsAnioBiciesto(2012)).toEqual("Es Anio Biciesto");
     });
-    it("deberia determinar si el anio 1800 es viciesto",()=>{
-        expect(EsAnioBiciesto(2016)).toEqual("No es Anio Biciesto");
+    it("deberia determinar si el anio 2016 es viciesto",()=>{
+        expect(EsAnioBiciesto(2016)).toEqual("Es Anio Biciesto");
     });
+    it("deberia determinar si el anio 2000 es viciesto",()=>{
+        expect(EsAnioBiciesto(2000)).toEqual("No es Anio Biciesto");
+    });
+    
 })
