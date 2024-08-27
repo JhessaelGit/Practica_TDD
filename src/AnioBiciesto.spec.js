4bc2823d2a267754ee08ba9a33af3e6d86a22a11
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-08-26 21:32:26 -0400

    La Prueba comprueba que si es divisible entre 100 no es anio biciesto y lo prueba

--- src/AnioBiciesto.spec.js
+++ src/AnioBiciesto.spec.js
@@ -1,6 +1,18 @@
 import EsAnioBiciesto from "./AnioBiciesto";
 describe("Anio Biciesto",()=>{
-    it("deberia determinar si el anio ingresado es viciesto",()=>{
-        expect(EsAnioBiciesto(1700)).toEqual("Es Anio Biciesto");
+    it("deberia determinar si el anio 1600 es viciesto",()=>{
+        expect(EsAnioBiciesto(1600)).toEqual("Es Anio Biciesto");
+    });
+    it("deberia determinar si el anio 1700 es viciesto",()=>{
+        expect(EsAnioBiciesto(1700)).toEqual("No es Anio Biciesto");
+    });
+    it("deberia determinar si el anio 1800 es viciesto",()=>{
+        expect(EsAnioBiciesto(1800)).toEqual("No es Anio Biciesto");
+    });
+    it("deberia determinar si el anio 1900 es viciesto",()=>{
+        expect(EsAnioBiciesto(1900)).toEqual("No es Anio Biciesto");
+    });
+    it("deberia determinar si el anio 2000 es viciesto",()=>{
+        expect(EsAnioBiciesto(2000)).toEqual("Es Anio Biciesto");
     });
 })
